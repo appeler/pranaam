2d181e437898e74a14b3fde93a4daa28865f2893
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2023-01-01 09:48:37 -0600

    Adding tarfile member sanitization to extractall() (#1)

--- pranaam/utils.py
+++ pranaam/utils.py
@@ -20,7 +20,26 @@ def download_file(url, target, file_name):
         open(file_path, "wb").write(r.content)
         # untar
         with tarfile.open(file_path, "r:gz") as tar_ref:
-            tar_ref.extractall(target)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_ref, target)
         # remove zip file
         os.remove(file_path)
         print("Finished downloading models ...")
